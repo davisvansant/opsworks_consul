cb2e86cc0be3936466cf15000d3a44752ba2a0a4
Davis Van Sant <@davisvansant>
2016-03-31 09:51:41 -0700

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'opsworks_consul'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email '@davisvansant'
+license          'All rights reserved'
+description      'Installs/Configures opsworks_consul'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
